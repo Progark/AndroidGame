c0fa58a63c60611e2389901fcacb72dd747a17ec
magnusjerre <magnus.jerre@gmail.com>
2013-02-27 15:15:11 +0100

    Fjernet unødvedige klasser
    Husk å fikse sheep.jar path, det er den som fører til rødt utropstegn

---
+++ Game/src/progark/pkg/game/OverallController.java
@@ -0,0 +1,7 @@
+package progark.pkg.game;
+
+import sheep.game.State;
+
+public class OverallController extends State {
+	//Meg f¿¿¿¿rst :P
+}
--- Game/src/progark/pkg/game/Test.java
+++
@@ -1,5 +0,0 @@
-package progark.pkg.game;
-
-public class Test {
-//Oh snap
-}
