d1888373866e0fe5aa0b1fd57e12dbb290a57c73
Bjorn Dolvik <bjorndolvik@gmail.com>
2013-04-08 09:25:19 +0200

    Endret bretthøyden

--- Game/src/progark/pkg/game/Board.java
+++ Game/src/progark/pkg/game/Board.java
@@ -5,7 +5,7 @@ import android.graphics.Canvas;
 public class Board {
 
 	public Board(int boardHeight, int boardWidth) {
-		
+		//TODO: boardheigh modulo figurstørrelse vil gi høyden, tilsvarende for vidden.
 	}
 	
 	public void draw(Canvas canvas){
